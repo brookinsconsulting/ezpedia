1c8c74c267bca70be291ef1d6ff39e2f0012643a
Daniel Beyer <ymc-dabe@vcs.ymc.ch>
2013-05-17 14:35:18 +0200

    Updated data-dump scripts to work in production

--- ezp/extension/ezpedia/bin/shell/exportMirrorVar.sh
+++ ezp/extension/ezpedia/bin/shell/exportMirrorVar.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 
+if [ $(id --user) -eq 0 ]; then
+  echo "INFO: Switching to user www-data..."
+  su www-data -c $0
+  exit $?
+fi
+
 # Package Command Variables
-packageCmd='tar -zcf';
+packageCmd='tar --dereference -zcf';
 site='ezpedia';
 dir='var';
 
@@ -14,13 +20,16 @@ dumpFilePackage="download/$site-$dir-$timestamp.tar.gz";
 rm -vrf var/autoload/ezp_*;
 rm -vrf var/cache/* var/plain_site/cache/* var/log/*;
 
+if [ ! -d "download" ]; then
+  mkdir download
+fi
+
 # Package Var Directory
 $packageCmd $dumpFilePackage $dir;
 
 # Regenerate autoloads
 php ./bin/php/ezpgenerateautoloads.php -o;
 php ./bin/php/ezpgenerateautoloads.php -e;
-php ./bin/php/ezpgenerateautoloads.php -k;
 
 # Inform the user
 echo "";
