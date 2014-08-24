00abccbb104ce39908a1ec5f9c996d64abb2a418
Brookins Consulting <info@brookinsconsulting.com>
2014-08-23 19:54:37 -0500

    Removed: Removed ezpublish 2013.4 release files

--- ezp/update/database/postgresql/5.0/unstable/dbupdate-5.0.0alpha1-to-5.0.0.sql
+++
@@ -1,4 +0,0 @@
-UPDATE ezsite_data SET value='5.0.0' WHERE name='ezpublish-version';
-UPDATE ezsite_data SET value='1' WHERE name='ezpublish-release';
-
-ALTER TABLE eznode_assignment ALTER COLUMN remote_id TYPE VARCHAR(100);
