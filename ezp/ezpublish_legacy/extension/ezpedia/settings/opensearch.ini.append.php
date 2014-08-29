554d369e0313aa3ae05751ea31ecca733ed01481
Brookins Consulting <info@brookinsconsulting.com>
2014-08-28 19:51:58 -0500

    Updated: Replaced url to opensearch plugin files to use github instead

--- ezp/ezpublish_legacy/extension/ezpedia/settings/opensearch.ini.append.php
+++ ezp/ezpublish_legacy/extension/ezpedia/settings/opensearch.ini.append.php
@@ -6,11 +6,11 @@ DescriptionList[]=ezpedia
 DescriptionList[]=ezpedia_google
 
 [OpenSearch_ezpedia]
-URL=http://pubsvn.ez.no/community/tools/browser_opensearch_plugins/ezpedia.xml
+URL=https://raw.githubusercontent.com/brookinsconsulting/opensearch/master/ezc/ezpedia.xml
 Title=eZpedia
 
 [OpenSearch_ezpedia_google]
-URL=http://pubsvn.ez.no/community/tools/browser_opensearch_plugins/ezpedia_via_google.xml
+URL=https://raw.githubusercontent.com/brookinsconsulting/opensearch/master/ezc/ezpedia_via_google.xml
 Title=eZpedia (via Google)
 
 */ ?>
