c45576a365673735451cd1b895e83ed7112f1da0
Brookins Consulting <info@brookinsconsulting.com>
2014-09-06 18:43:42 -0500

    Added: Example static cache configuration (disabled) for eZpedia

--- ezp/ezpublish_legacy/extension/ezpedia/settings/settings/override/site.ini.append.php
+++ ezp/ezpublish_legacy/extension/ezpedia/settings/settings/override/site.ini.append.php
@@ -1,5 +1,8 @@
 <?php /* #?ini charset="utf-8"?
 
+[ContentSettings]
+StaticCache=disabled
+
 [DebugSettings]
 DebugOutput=disabled
 
--- ezp/ezpublish_legacy/extension/ezpedia/settings/settings/override/staticcache.ini.append.php
+++ ezp/ezpublish_legacy/extension/ezpedia/settings/settings/override/staticcache.ini.append.php
@@ -1,16 +1,39 @@
 <?php /*
 
 [CacheSettings]
-HostName=pubsvn.ez.no/wiki
+HostName=
 StaticStorageDir=static
 MaxCacheDepth=10
 CachedURLArray[]
+CachedURLArray[]=/
 CachedURLArray[]=/ez
 CachedURLArray[]=/ez/*
 CachedURLArray[]=/about
 CachedURLArray[]=/about/*
 CachedURLArray[]=/people
 CachedURLArray[]=/people/*
+CachedURLArray[]=/solution
+CachedURLArray[]=/solution/*
+CachedURLArray[]=/snippet
+CachedURLArray[]=/snippet/*
+CachedURLArray[]=/project
+CachedURLArray[]=/project/*
+CachedURLArray[]=/new_stack
+CachedURLArray[]=/new_stack/*
+CachedURLArray[]=/help
+CachedURLArray[]=/help/*
+CachedURLArray[]=/new
+CachedURLArray[]=/new/*
+CachedURLArray[]=/about
+CachedURLArray[]=/about/*
+CachedURLArray[]=/minutes
+CachedURLArray[]=/minutes/*
+CachedURLArray[]=/learning
+CachedURLArray[]=/learning/*
+CachedURLArray[]=/discussion
+CachedURLArray[]=/discussion/*
+CachedURLArray[]=/updated
+CachedURLArray[]=/updated/*
 
 # A list of site accesses to generate static content for
 CachedSiteAccesses[]
@@ -21,4 +44,5 @@ CachedSiteAccesses[]=fr
 # can NOT use a wildcard here.
 AlwaysUpdateArray[]
 AlwaysUpdateArray[]=/
+
 */ ?>
