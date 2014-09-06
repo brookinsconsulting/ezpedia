c8882d2636ccee1fdbb400de673879cfb312a4d6
Brookins Consulting <info@brookinsconsulting.com>
2014-09-06 12:35:24 -0500

    Added: owsimpleoperator configuration for ezpedia custom operators. Switched to new operator and deprecated wrapoperator usage

---
+++ ezp/ezpublish_legacy/extension/ezpedia/classes/ezpediasimpleoperators.php
@@ -0,0 +1,60 @@
+<?php
+
+class eZpediaSimpleOperators extends OWSimpleOperator
+{
+    /*!
+     * Quick and dirty way to get the issue number from a git repository commit message atom feed
+     */
+    function getIssueFromGitCommitMessage( $data )
+    {
+        //given string $data, will return the first $issue string in that string
+	$ret = false;
+        $limit = 5;
+
+        // test title first
+	$splitTitle = preg_split( "/EZP-/", $data['title'] );
+
+	if( isset( $splitTitle[1] ) )
+          $splitTitleTestForZero = preg_split( "/0/", $splitTitle[1] );
+	else
+          $splitTitleTestForZero = null;
+
+	if( $splitTitleTestForZero[0] == 0 ) { $limit = 6; } else { $limit = 5; }
+
+        // test message second
+        $splitMessage = preg_split( "/EZP-/", $data['content'] );
+
+        // eZDebug::writeDebug( "wrap_operator: getIssueFromPubSVNCommitMessage, results: " . print_r( $splitMessage, TRUE) );
+
+	if( isset( $splitMessage[1] ) )
+          $splitMessageTestForZero = preg_split( "/0/", $splitMessage[1] );
+	else
+          $splitMessageTestForZero = null;
+
+	if( $splitMessageTestForZero[0] == 0 ) { $limit = 6; } else { $limit = 5; }
+
+	if( isset( $splitTitle[1] ) ) {
+	    $match = $splitTitle[1];
+            $issue = substr( $match, 0, +$limit );
+
+	    if ( $issue != '' && count( $issue ) <= 5 && is_numeric( $issue ) ) {
+               // eZDebug::writeDebug( "wrap_operator: getIssueFromPubSVNCommitMessage, results: " . print_r( $issue, TRUE) );
+               $ret = $issue;
+	    }
+	}
+	elseif( isset( $splitMessage[1] ) ) {
+	    $match = $splitMessage[1];
+    	    $issue = substr( $match, 0, +$limit );
+
+	    if ( $issue != '' && count( $issue ) <= 5 && is_numeric( $issue ) ) {
+               // eZDebug::writeDebug( "wrap_operator: getIssueFromPubSVNCommitMessage, results: " . print_r( $issue, TRUE) );
+ 	       $ret = $issue;
+	    }
+	}
+
+        return $ret;
+    }
+
+}
+
+?>
---
+++ ezp/ezpublish_legacy/extension/ezpedia/settings/settings/override/owsimpleoperator.ini.append.php
@@ -0,0 +1,23 @@
+<?php /* #?ini charset="utf-8"?
+
+# Names of system functions that
+# are usable as template operators
+
+[PHPFunctions]
+PermittedFunctionList[]
+PermittedFunctionList[]=time
+PermittedFunctionList[]=mktime
+PermittedFunctionList[]=getdate
+PermittedFunctionList[]=str_replace
+PermittedFunctionList[]=ereg_replace
+PermittedFunctionList[]=str_rot13
+PermittedFunctionList[]=file_get_contents
+
+# Class file path, Class Name, Class Method
+# that are useable as template operators
+
+[ClassOperators]
+PermittedClassOperatorList[]
+PermittedClassOperatorList[]=extension/ezpedia/classes/ezpediasimpleoperators.php;eZpediaSimpleOperators;getIssueFromGitCommitMessage
+
+*/ ?>
