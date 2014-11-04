5e5a3fcaa9b065d5ee5c287b6dc69da121876c3a
Brookins Consulting <info@brookinsconsulting.com>
2014-11-04 03:20:44 -0600

    Updated: Updating ini settings configuration to restore view count feature

--- ezp/ezpublish_legacy/extension/ezpedia/settings/settings/override/logfile.ini.append.php
+++ ezp/ezpublish_legacy/extension/ezpedia/settings/settings/override/logfile.ini.append.php
@@ -1,15 +1,13 @@
 <?php /*
 
 [AccessLogFileSettings]
-# StorageDir=./
-StorageDir=/var/log/apache2/ezpedia/
-# LogFileName=ezpsvn_concatenated_access_log
-LogFileName=access_log
+StorageDir=/data/ezpedia-ezpublish/www-data/logs/apache/
+LogFileName=ezpedia_access.log
 SitePrefix[]
-SitePrefix[]=wiki\/de\/
-SitePrefix[]=wiki\/en\/
-SitePrefix[]=wiki\/es\/
-SitePrefix[]=wiki\/fr\/
-SitePrefix[]=wiki\/nl\/
+SitePrefix[]=\/de\/
+SitePrefix[]=\/en\/
+SitePrefix[]=\/es\/
+SitePrefix[]=\/fr\/
+SitePrefix[]=\/nl\/
 
 */ ?>
