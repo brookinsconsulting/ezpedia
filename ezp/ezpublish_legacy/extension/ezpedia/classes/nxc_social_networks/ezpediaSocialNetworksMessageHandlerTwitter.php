5edd6ef3944a8b1e84644309128f054700e20525
Brookins Consulting <info@brookinsconsulting.com>
2015-06-11 02:48:49 -0500

    Updated: extended handler to support generic tweeting about most common content types more accurately

--- ezp/ezpublish_legacy/extension/ezpedia/classes/nxc_social_networks/ezpediaSocialNetworksMessageHandlerTwitter.php
+++ ezp/ezpublish_legacy/extension/ezpedia/classes/nxc_social_networks/ezpediaSocialNetworksMessageHandlerTwitter.php
@@ -11,7 +11,27 @@ class ezpediaSocialNetworksMessageHandlerDefault extends nxcSocialNetworksMessag
     public static function message( $publishHandler, eZContentObject $object, $message, $messageLength = null, $options ) {
         $url = false;
 
-        $messageIntro = 'The article "';
+        if( $object->attribute( 'class_identifier' ) == 'article'
+            || $object->attribute( 'class_identifier' ) == 'wiki_article' )
+        {
+            $contentType = 'article';
+        }
+        elseif( $object->attribute( 'class_identifier' ) == 'wiki_namespace' )
+        {
+            $contentType = 'namespace';
+        }
+        elseif( $object->attribute( 'class_identifier' ) == 'forum_reply'
+                ||  $object->attribute( 'class_identifier' ) == 'forum_topic'
+                ||  $object->attribute( 'class_identifier' ) == 'forum' )
+        {
+            $contentType = 'forum post';
+        }
+        else
+        {
+            $contentType = 'article';
+        }
+
+        $messageIntro = 'The ' . $contentType . ' "';
         $messageBody = $message;
         $messageEnd = ( $object->attribute( 'current_version' ) == 1 ) ? '" was published.' : '" was updated.';
         $messageOutro = " #ezpublish";
