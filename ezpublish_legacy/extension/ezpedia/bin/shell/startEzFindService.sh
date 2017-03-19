f16dc0af569314255af3adf8c53134daa2d6ffe1
Brookins Consulting <info@brookinsconsulting.com>
2017-03-19 07:09:18 -0400

    Updated: Migrated path to new host

--- ezpublish_legacy/extension/ezpedia/bin/shell/startEzFindService.sh
+++ ezpublish_legacy/extension/ezpedia/bin/shell/startEzFindService.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-screen -d -m sh -c "while :; do cd /home/brookins/ezpedia/java; java -server -d64 -Xmx1500m -Xms1500m -XX:+UseParallelGC -XX:+AggressiveOpts -XX:NewRatio=5 -jar start.jar; done;"
+screen -d -m sh -c "while :; do cd /web/doc/vh/ezpedia/ezpedia.org/vhosts/www.ezpedia.org/ezpublish_legacy/extension/ezfind/java; java -server -d64 -Xmx1500m -Xms1500m -XX:+UseParallelGC -XX:+AggressiveOpts -XX:NewRatio=5 -jar start.jar; done;"
 
 exit;
